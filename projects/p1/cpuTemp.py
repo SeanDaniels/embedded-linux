f33b2f7e8a4c74ca1d3bb412238ef47902145171
Sean Daniels <sdaniels8864@gmail.com>
2021-02-15 10:43:25 -0500

    fixed temperature reading

--- projects/p1/cpuTemp.py
+++ projects/p1/cpuTemp.py
@@ -6,20 +6,25 @@ from time import sleep, strftime, time
 import datetime as dt
 
 # Turn interactive mode on
-plt.ion()
 fig = plt.figure()
-ax = fig.add_subplot()
+ax = fig.add_subplot(1,1,1)
 x = []
 y = []
 
-def animate(i, x: list, y: list) -> None:
-    currentTemp = CPUTemperature()
+def animate(i, x, y):
+    animateStart = time()
+    currentTemp = CPUTemperature().temperature
     x.append(dt.datetime.now().strftime('%H:%M:%S.%F'))
     y.append(currentTemp)
     x = x[-20:]
     y = y[-20:]
     ax.clear()
     ax.plot(x,y)
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('Temperature Over Time')
+    plt.ylabel('Temperature (deg C)')
+    print("Function runtimed: {}".format(time()-animateStart))
 
 ani = animation.FuncAnimation(fig, animate, fargs=(x,y), interval=1000)
 plt.show()
