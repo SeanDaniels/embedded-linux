29e9b60a2bc3fa5ee5be792dbe95c7b5ba456cd1
Sean Daniels <sdaniels8864@gmail.com>
2021-02-15 12:19:00 -0500

    Updated cpuTemp.py
    Added function to get cpu frequency throug subprocess call
    
    Plotting isn't formatted correctly

--- projects/p1/cpuTemp.py
+++ projects/p1/cpuTemp.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os, subprocess, shlex
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from gpiozero import CPUTemperature
@@ -7,24 +8,60 @@ import datetime as dt
 
 # Turn interactive mode on
 fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-x = []
-y = []
+temperatureAx = fig.add_subplot(2,1,1)
+freqAx = fig.add_subplot(2,1,2)
+frequencyCommand = "cpufreq-info -f"
+frequencyCommandArgs = shlex.split(frequencyCommand)
+xTemp = []
+yTemp = []
+xFreq = []
+yFreq = []
+animateRunTime = []
 
-def animate(i, x, y):
+# Shows cpu temperature
+# Need to add frequency
+# Need to add voltage
+# Adding frequency now
+def getFrequency():
+    data, temp = os.pipe()
+    os.close(temp)
+    s = subprocess.check_output(frequencyCommandArgs, stdin=data)
+    freqValue = int(s.decode("utf-8"))
+    return freqValue
+
+def animateTemperaturePlot(i, x, y):
     animateStart = time()
     currentTemp = CPUTemperature().temperature
-    x.append(dt.datetime.now().strftime('%H:%M:%S.%F'))
+    x.append(dt.datetime.now().strftime('%H:%M:%S'))
     y.append(currentTemp)
     x = x[-20:]
     y = y[-20:]
-    ax.clear()
-    ax.plot(x,y)
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title('Temperature Over Time')
-    plt.ylabel('Temperature (deg C)')
-    print("Function runtimed: {}".format(time()-animateStart))
+    temperatureAx.clear()
+    temperatureAx.plot(x,y)
+    # plt.xticks(rotation=45, ha='right')
+    # plt.subplots_adjust(bottom=0.30)
+    # plt.title('Temperature Over Time')
+    temperatureAx.set_xticks(visible=False)
+    temperatureAx.set_ylabel('Temperature (deg C)')
+    temperatureAx.grid(True)
+    animateRunTime.append(time()-animateStart)
 
-ani = animation.FuncAnimation(fig, animate, fargs=(x,y), interval=1000)
+def animateFreqPlot(i,x,y):
+    animateStart = time()
+    currentFreq = getFrequency()
+    x.append(dt.datetime.now().strftime('%H:%M:%S'))
+    y.append(currentFreq/1000000)
+    x = x[-20:]
+    y = y[-20:]
+    freqAx.clear()
+    freqAx.plot(x,y)
+    # plt.xticks(rotation=45, ha='right')
+    # plt.subplots_adjust(bottom=0.30)
+    freqAx.set_ylabel('Frequency (MHz)')
+    freqAx.set_xticks(visible=False)
+    freqAx.grid(True)
+    animateRunTime.append(time()-animateStart)
+
+aniTemp = animation.FuncAnimation(fig, animateTemperaturePlot, fargs=(xTemp,yTemp), interval=1000)
+aniFreq = animation.FuncAnimation(fig, animateFreqPlot, fargs=(xFreq,yFreq), interval=1000)
 plt.show()
